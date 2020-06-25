32f0074ef8758ad0d5ca032293f1e57ce17d8aa0
Ben Purdy <benpurdy@Bens-Air.mediacom.info>
2020-06-25 09:48:35 -0500

    long live my first bash script

---
+++ _posts/2020-06-25-Ready-to-be-back.markdown
@@ -0,0 +1,10 @@
+---
+layout: post
+title:  "Ready to be back"
+date:   2020-06-25-09:30:13 -0500 
+categories: jekyll update
+---
+
+Coming back to everything this week is a lot. I enjoyed colorado, but im ready to tackle it my workload and made some progress. Yesterday I was able to fix the surface level issues with the state machine implementation. I am sure there will be other issues, but I got Megan to move through repeated calls to coroutines.  
+
+	Immediately, I haven't been able to make much progress on the space syntax paragraph with everything happening. Today's goal is to do some research and get something together for Dr. Gilbert.
---
+++ _posts/createpost.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# My first script
+ 
+# jekyll bash script to create blank post files 
+# syntax ./createpost.sh [name of file]
+# syntax ./createpost.sh [name of file] -r
+# name of the file will also be the title of the post, -r removes all white space 
+FileName=""
+if [ $# -eq 0 ]
+  then
+  	#statements
+    FileName="$(date '+%Y-%m-%d')-defult.markdown"
+else 
+	#statements
+	FileName="$(date '+%Y-%m-%d')-$1.markdown"
+fi
+
+echo "created file $FileName" 
+#create file 
+if ! [[ -f $FileName ]] 
+	then 
+touch $FileName
+#inset into the file
+title=""
+if [ $2 == "-r" ]
+then 
+	echo $title
+	for ((i=1;i<=${#1};i++)); do
+	  	if [ ${1:$i-1:1} = "-" ]
+		then	
+			title+=" "
+		else
+			title+="${1:$i-1:1}"
+		fi
+	done
+	echo "$title"
+else
+	title= $1
+fi
+
+echo "---
+layout: post
+title:  \""$title"\"
+date:   "$(date '+%Y-%m-%d-%T')" -0500 
+categories: jekyll update
+---" >> $FileName
+else
+	echo "File already exits"
+fi
